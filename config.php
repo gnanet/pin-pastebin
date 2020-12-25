8e328ddb1d23c776c0d3d4c8030fd7cbab59a5ca
Gergely Nagy <gna@r-us.hu>
2020-12-25 23:09:08 +0100

    prepare curl usage

--- config.php
+++ config.php
@@ -77,3 +77,16 @@ function db() {
 
     return $db;
 }
+
+function file_get_contents_curl($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36");
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
+
